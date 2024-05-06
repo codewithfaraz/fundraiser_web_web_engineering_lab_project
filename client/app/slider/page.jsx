7a5cf40adebea6ec7e4caefac73c03944bc9c3d5
kashannadeeem <97223160+kashannadeeem@users.noreply.github.com>
2024-05-06 11:59:21 +0500

    slider

--- client/app/slider/page.jsx
+++ client/app/slider/page.jsx
@@ -1,49 +1,65 @@
 import React from 'react';
+import classes from "./slider-style.module.css";
 
-export default function Slider() {
+
+
+
+const CustomNextArrow = (props) => {
+    const { onClick } = props;
     return (
-        <div className="w-3/4 m-auto">
-            <div className="mt-20">
-                {data.map((d)=>(
-                 <div>
-<                   div>
-                     <img src={d.img} alt=""/>
+        <button className={classes.nextButton} onClick={onClick}>
+            Next
+        </button>
+    );
+};
+export default function Slider() {
+    const data = [
+        {
+            name: 'Kashan Nadeem',
+            Description: 'i am kashan nadeem. Recently study in FAST University',
+            img: '/Projects Picture/img1.jpg'
+        },
+        {
+            name: 'Dawood Ramazan',
+            img: '/Projects Picture/img2.jpg',
+            Description: 'i am Dawood Ramazan. Recently study in FAST University',
+        },
+        {
+            name: 'Faraz Maqsood',
+            img: '/Projects Picture/img3.jpg',
+            Description: 'i am Faraz Maqsood. Recently study in FAST University',
+        },
+        {
+            name: 'Hassan Zahid',
+            img: '/Projects Picture/img4.jpg',
+            Description: 'i am Hassan Zahid. Recently study in FAST University',
+        },
+    ];
 
-                </div>
-    <div>
-    <p>{d.name}</p>
-    <p>{d.reviews}</p>
-    </div>
-    </div>  
-                ))}
+    const settings = {
+        dots: false,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3, // Show three items at a time
+        slidesToScroll: 1,
+        nextArrow: <CustomNextArrow />,
+    };
 
-              
-            </div>
+    return (
+        <div className={classes.slider}>
+            <h1>Image Slider</h1>
+                
             
+                {data.map((item, index) => (
+                    <div key={index}>
+                        <img src={item.img} alt={item.name} />
+                        <div>
+                            <h3>{item.name}</h3>
+                            <p>{item.Description}</p>
+                            <button>View</button>
+                        </div>
+                    </div>
+                ))}
         </div>
     );
 }
-
-const data=[
-    {
-        name:'Kashan Nadeem',
-        Description:'i am kashan nadeem. Recently study in FAST University',
-        img:'/Projects Picture/img1.jpg'
-    },
-
-    {
-        name:'Dawood Ramazan',
-        img:'/Projects Picture/img2.jpg',
-        Description:'i am Dawood Ramazan. Recently study in FAST University',
-    },
-    {
-        name:'Faraz Maqsood',
-        img:'/Projects Picture/img3.jpg',
-         Description:'i am Faraz Maqsood. Recently study in FAST University',
- },
- {
-           name:'Hassan Zahid',
-           img:'/Projects Picture/img4.jpg',
-           Description:'i am Hassan Zahid. Recently study in FAST University',
-       },
-    ];
