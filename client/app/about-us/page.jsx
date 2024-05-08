cd38f774234d604f1a952b2e869f06f0f6c6903f
Daud772 <dawoodramzan772@gmail.com>
2024-05-08 11:41:55 +0500

    About Us Finalize

--- client/app/about-us/page.jsx
+++ client/app/about-us/page.jsx
@@ -1,71 +1,201 @@
- import Foot from "@/components/Footer";
- import Footer from "rc-footer";
- import "rc-footer/assets/index.css";
- import classes from "./about-us.module.css";
- const TeamMember = ({ name, role, description, email }) => (
+import Foot from "@/components/Footer";
+import Footer from "rc-footer";
+import "rc-footer/assets/index.css";
+import classes from "./about-us.module.css";
+const TeamMember = ({ name, role, description, email }) => (
   <div className="column">
     <div className="card">
-      <img src={`/w3images/${name.toLowerCase().replace(' ', '')}.jpg`} alt={name} style={{ width: '100%' }} />
+      <img
+        src={`/w3images/${name.toLowerCase().replace(" ", "")}.jpg`}
+        alt={name}
+        style={{ width: "100%" }}
+      />
       <div className="container">
         <h2>{name}</h2>
         <p className="title">{role}</p>
         <p>{description}</p>
         <p>{email}</p>
-        <p><button className="button">Contact</button></p>
+        <p>
+          <button className="button">Contact</button>
+        </p>
       </div>
     </div>
   </div>
 );
 
-export default function About() {return(
-  <div>
-    <div style={{ padding: '50px', textAlign: 'center', backgroundColor: '#474e5d', color: 'white' }}>
-      <h1>About Us </h1>
-      <p>Meet our talented individuals who bring passion and expertise to everything they do.</p>
-      <p> From creative designers to full-stack developers</p>
-    </div>
-    <h2 style={{ textAlign: 'center' }}>Our Team</h2>
-    <div style={{ content: "", clear: 'both', display: 'table' }}>
-      <div style={{ float: 'left', width: '25.3%', marginBottom: '16px', padding: '0 8px',marginLeft:"90px" }}>
-        <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', margin: '8px' }}>
-          <img src="/assets/download.png" alt="Jane" style={{ width: '100%' }} />
-          <div style={{ padding: '0 16px' }}>
-            <h2>Faraz Maqsood</h2>
-            <p style={{ color: 'grey' }}>CEO & Founder</p>
-            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-            <p>FarazMaqsood@example.com</p>
-            <p><button style={{ border: 'none', outline: '0', display: 'inline-block', padding: '8px', color: 'white', backgroundColor: '#000', textAlign: 'center', cursor: 'pointer', width: '100%' }}>Contact</button></p>
+export default function About() {
+  return (
+    <div>
+      <div
+        style={{
+          padding: "50px",
+          textAlign: "center",
+          backgroundColor: "#474e5d",
+          color: "white",
+        }}
+      >
+        <h1>About Us </h1>
+        <p>
+          Meet our talented individuals who bring passion and expertise to
+          everything they do.
+        </p>
+        <p> From creative designers to full-stack developers</p>
+      </div>
+      <h2 style={{ textAlign: "center" }}>Our Team</h2>
+      <div style={{ content: "", clear: "both", display: "table" }}>
+        <div
+          style={{
+            float: "left",
+            width: "25.3%",
+            marginBottom: "16px",
+            padding: "0 8px",
+            marginLeft: "90px",
+          }}
+        >
+          <div
+            style={{
+              boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+              margin: "8px",
+            }}
+          >
+            <img
+              src="/assets/download.png"
+              alt="Jane"
+              style={{ width: "100%" }}
+            />
+            <div style={{ padding: "0 16px" }}>
+              <h2>Faraz Maqsood</h2>
+              <p style={{ color: "grey" }}>CEO & Founder</p>
+              <p>
+                At[ Growth Grant], we are driven by a passion for innovation and
+                a commitment to excellence, striving to redefine standards and
+                exceed expectations in everything we do and what we want
+              </p>
+              <p>FarazMaqsood@example.com</p>
+              <p>
+                <button
+                  style={{
+                    border: "none",
+                    outline: "0",
+                    display: "inline-block",
+                    padding: "8px",
+                    color: "white",
+                    backgroundColor: "#000",
+                    textAlign: "center",
+                    cursor: "pointer",
+                    width: "100%",
+                  }}
+                >
+                  Contact
+                </button>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            float: "left",
+            width: "25.3%",
+            marginBottom: "16px",
+            padding: "0 8px",
+            marginLeft: "90px",
+          }}
+        >
+          <div
+            style={{
+              boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+              margin: "8px",
+            }}
+          >
+            <img
+              src="/assets/download.png"
+              alt="Mike"
+              style={{ width: "100%" }}
+            />
+            <div style={{ padding: "0 16px" }}>
+              <h2>Dawood Ramzan</h2>
+              <p style={{ color: "grey" }}>Developer</p>
+              <p>
+                At [Growth Grant], our talented team of developers harness
+                cutting-edge technologies and their creative expertise to craft
+                seamless digital experiences that empower and inspire users
+                worldwide
+              </p>
+              <p>Dawood@example.com</p>
+              <p>
+                <button
+                  style={{
+                    border: "none",
+                    outline: "0",
+                    display: "inline-block",
+                    padding: "8px",
+                    color: "white",
+                    backgroundColor: "#000",
+                    textAlign: "center",
+                    cursor: "pointer",
+                    width: "100%",
+                  }}
+                >
+                  Contact
+                </button>
+              </p>
+            </div>
+          </div>
+        </div>
+        <div
+          style={{
+            float: "left",
+            width: "25.3%",
+            marginBottom: "16px",
+            padding: "0 8px",
+            marginLeft: "90px",
+          }}
+        >
+          <div
+            style={{
+              boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2)",
+              margin: "8px",
+            }}
+          >
+            <img
+              src="/assets/download.png"
+              alt="John"
+              style={{ width: "100%" }}
+            />
+            <div style={{ padding: "0 16px" }}>
+              <h2>Kashan Nadeem</h2>
+              <p style={{ color: "grey" }}> UI/UX Designer</p>
+              <p>
+                At [Growth Grant], our UI/UX specialists blend artistry with
+                functionality, crafting intuitive interfaces that captivate
+                users and elevate their digital journey, ensuring every
+                interaction is both delightful and meaningful
+              </p>
+              <p>KashanNadeem@example.com</p>
+              <p>
+                <button
+                  style={{
+                    border: "none",
+                    outline: "0",
+                    display: "inline-block",
+                    padding: "8px",
+                    color: "white",
+                    backgroundColor: "#000",
+                    textAlign: "center",
+                    cursor: "pointer",
+                    width: "100%",
+                  }}
+                >
+                  Contact
+                </button>
+              </p>
+            </div>
           </div>
         </div>
       </div>
-      <div style={{ float: 'left', width: '25.3%', marginBottom: '16px', padding: '0 8px',marginLeft:"90px" }}>
-        <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', margin: '8px' }}>
-          <img src="/assets/download.png" alt="Mike" style={{ width: '100%' }} />
-          <div style={{ padding: '0 16px' }}>
-            <h2>Dawood Ramzan</h2>
-            <p style={{ color: 'grey' }}>Developer</p>
-            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-            <p>Dawood@example.com</p>
-            <p><button style={{ border: 'none', outline: '0', display: 'inline-block', padding: '8px', color: 'white', backgroundColor: '#000', textAlign: 'center', cursor: 'pointer', width: '100%' }}>Contact</button></p>
-          </div>
-        </div>
-      </div>
-      <div style={{ float: 'left', width: '25.3%', marginBottom: '16px', padding: '0 8px',marginLeft:"90px"}}>
-        <div style={{ boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)', margin: '8px' }}>
-          <img src="/assets/download.png" alt="John" style={{ width: '100%' }} />
-          <div style={{ padding: '0 16px' }}>
-            <h2>Kashan Nadeem</h2>
-            <p style={{ color: 'grey' }}> UI/UX Designer</p>
-            <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-            <p>KashanNadeem@example.com</p>
-            <p><button style={{ border: 'none', outline: '0', display: 'inline-block', padding: '8px', color: 'white', backgroundColor: '#000', textAlign: 'center', cursor: 'pointer', width: '100%' }}>Contact</button></p>
-          </div>
-        </div>
-      </div>
-    </div>
 
-    <Footer
-         className={classes.back}
+      <Footer
+        className={classes.back}
         columns={[
           {
             title: (
@@ -108,7 +238,6 @@ export default function About() {return(
         ]}
         bottom="All Rights Are Reserved"
       />
-  </div>
-);
-
+    </div>
+  );
 }
